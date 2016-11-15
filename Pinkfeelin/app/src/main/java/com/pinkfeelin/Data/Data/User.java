c753bbab081f61801cb62e35969fb1ea30748eb1
marcoleon <paramore_malj@outlook.com>
2016-11-15 13:28:16 -0600

    user authentification

---
+++ Pinkfeelin/app/src/main/java/com/pinkfeelin/Data/Data/User.java
@@ -0,0 +1,98 @@
+package com.pinkfeelin.Data.Data;
+
+/**
+ * Created by Marco on 15/11/2016.
+ */
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String lastname;
+    private String telefono;
+    private int tipo;
+    private String sexo;
+    private String fecha_nac;
+    private String created_at;
+    private String updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(String fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
--- Pinkfeelin/app/src/main/java/com/pinkfeelin/Login/Login.java
+++ Pinkfeelin/app/src/main/java/com/pinkfeelin/Login/Login.java
@@ -1,14 +1,22 @@
 package com.pinkfeelin.Login;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonWriter;
+import com.pinkfeelin.Data.Data.Repository.UserRepository;
+import com.pinkfeelin.Data.Data.User;
 import com.pinkfeelin.R;
+import com.pinkfeelin.Util.NetUtil;
+
+import java.util.HashMap;
 
 public class Login extends AppCompatActivity {
     EditText email,password;
@@ -18,7 +26,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Gson gson= new Gson();
+        final Gson gson= new Gson();
         email=(EditText) findViewById(R.id.email);
         password=(EditText) findViewById(R.id.password);
         loginbtn=(Button) findViewById(R.id.loginbtn);
@@ -29,6 +37,18 @@ public class Login extends AppCompatActivity {
                 em = email.getText().toString();
                 pass = password.getText().toString();
                 Toast.makeText(Login.this,"Hola "+em+" "+pass,Toast.LENGTH_SHORT).show();
+                UserRepository.login(em, pass, new UserRepository.UserCallback() {
+                    @Override
+                    public void error(String msg) {
+                        Toast.makeText(Login.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void succes(boolean succes, User user) {
+                        Toast.makeText(Login.this, "Hola "+user.getName(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
             }
         });
