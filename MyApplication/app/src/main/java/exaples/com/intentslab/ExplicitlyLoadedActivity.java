70fee068dde7b303350c95eab6973f71b3fd2043
marcoleon <paramore_malj@outlook.com>
2016-11-23 11:16:50 -0600

    Actividades de lab

---
+++ MyApplication/app/src/main/java/exaples/com/intentslab/ExplicitlyLoadedActivity.java
@@ -0,0 +1,13 @@
+package exaples.com.intentslab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExplicitlyLoadedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explicitly_loaded);
+    }
+}
